994d44fc4d8f7fd5e2c0c4c16a5d5fbe16291528
devkamil <kamilkarwowski.dev@gmail.com>
2017-11-07 14:21:13 +0100

    First version - displaying current directory

---
+++ src/main/java/pl/devkamil/app/Main.java
@@ -0,0 +1,9 @@
+package pl.devkamil.app;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        System.out.println("Working Directory: " + System.getProperty("user.dir"));
+    }
+}
