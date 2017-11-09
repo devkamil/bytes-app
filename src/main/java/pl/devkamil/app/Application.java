7c92a797c46908d7bffcc4b4697e32a55b61b75a
devkamil <kamilkarwowski.dev@gmail.com>
2017-11-09 14:31:41 +0100

    Migrate to Spring Boot framework

--- src/main/java/pl/devkamil/app/Application.java
+++ src/main/java/pl/devkamil/app/Application.java
@@ -1,18 +1,25 @@
 package pl.devkamil.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import pl.devkamil.app.service.ByteService;
 
+@SpringBootApplication
 public class Application {
 
 
     public static void main(String[] args) {
 
-        ByteService byteService = new ByteService();
+        SpringApplication.run(Application.class, args);
+
+
+
+//        ByteService byteService = new ByteService();
 
 //        System.out.println("Working Directory: " + System.getProperty("user.dir"));
 
-        byteService.readFile("FileToRead11.txt");
-        byteService.writeFile("FileToRead111.txt");
+//        byteService.readFile("FileToRead11.txt");
+//        byteService.writeFile("FileToRead111.txt");
 
     }
 
---
+++ src/main/java/pl/devkamil/app/controller/ByteController.java
@@ -0,0 +1,24 @@
+package pl.devkamil.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.devkamil.app.service.ByteService;
+
+@RestController
+@RequestMapping("/")
+public class ByteController {
+
+    @Autowired
+    private ByteService byteService;
+
+    @GetMapping("/{filePath}")
+    public String readFile (@PathVariable ("filePath") String filePath){
+        return byteService.readFile(filePath + ".txt");
+    }
+
+    @PostMapping("/{filePath}")
+    public void write (@PathVariable ("filePath") String filePath){
+        byteService.writeFile(filePath + ".txt");
+    }
+
+}
